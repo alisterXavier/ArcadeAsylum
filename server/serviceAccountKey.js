a05fc119b0cd7dd4d277d8f408d7d0f1a3468478
Alister <xavieralister153@gmail.com>
2023-02-28 20:02:51 +0400

    env

--- server/serviceAccountKey.js
+++ server/serviceAccountKey.js
@@ -1,11 +1,11 @@
 module.exports = {
   type: "service_account",
   project_id: "arcadeasylum-46375",
-  private_key_id: process.env.Private_key_id,
-  private_key: process.env.Private_Key.replace(/\\n/g, "\n"),
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email:
     "firebase-adminsdk-ej7f2@arcadeasylum-46375.iam.gserviceaccount.com",
-  client_id: process.env.Client_id,
+  client_id: process.env.CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
